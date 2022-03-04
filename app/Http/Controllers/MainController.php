0fc00593f9996904eb17205f0f79e449acc28cdc
Mahmoud Hammad <mahmoudhammad423@gmail.com>
2022-03-04 17:52:41 +0200

    edit

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,23 +7,25 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+    private $url = 'https://staging.mazaady.com/api';
+
     public function get_all_cats(Client $client){
-        $res = $client->request('GET', 'https://staging.mazaady.com/api/get_all_cats');
+        $res = $client->request('GET', $this->url.'/get_all_cats');
         return $res->getBody();
     }
 
     public function get_sub_cats(Client $client, $id){
-        $res = $client->request('GET', 'https://staging.mazaady.com/api/get_all_cats/');
+        $res = $client->request('GET', $this->url.'/get_all_cats/');
         return $res->getBody();
     }
 
     public function get_models(Client $client, $id){
-        $res = $client->request('GET', 'https://staging.mazaady.com/api/get-options-child/'.$id);
+        $res = $client->request('GET', $this->url.'/get-options-child/'.$id);
         return $res->getBody();
     }
 
     public function get_properties(Client $client, $id){
-        $res = $client->request('GET', 'https://staging.mazaady.com/api/properties?cat='.$id);
+        $res = $client->request('GET', $this->url.'/properties?cat='.$id);
         return $res->getBody();
     }
 }
